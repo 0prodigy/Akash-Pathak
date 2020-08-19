e920739b2ac0dcac41af60f6781cd21745a2ac36
Akash <pathakvikash9211@gmail.com>
2020-08-19 21:13:31 +0530

    Update skill section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,51 +1,64 @@
 import React from "react";
 import styled from "styled-components";
+import { CarouselProvider, Slider, Slide, Image } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 const Wrapper = styled.div`
-  padding: 20px;
-  .row {
+  padding: 40px 20px;
+
+  Slide {
+    color: #fff;
+  }
+  img {
+    filter: grayscale(1);
     width: 100%;
-    display: flex;
-    position: relative;
-    flex-warap: wrap;
-    justify-content: center;
-    > div {
-      margin: 10px 30px;
-      width: 250px;
-      img {
-        width: 100%;
-        filter: grayscale(1);
-      }
-    }
+    padding: 0 10px;
   }
 `;
 function Skills() {
   return (
     <Wrapper>
-      <div className="row">
-        <div>
-          <img src="img/caret-down.svg" alt="" />
-        </div>
-        <div>
-          <img src="img/caret-down.svg" alt="" />
-        </div>
-        <div>
-          <img src="img/caret-down.svg" alt="" />
-        </div>
-      </div>
-      <div className="row">
-        <div>
-          <img src="img/caret-down.svg" alt="" />
-        </div>
-        <div>
-          <img src="img/caret-down.svg" alt="" />
-        </div>
-      </div>
-      <div className="row">
-        <div>
-          <img src="img/caret-down.svg" alt="" />
-        </div>
-      </div>
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={100}
+        totalSlides={10}
+        isPlaying={true}
+        interval={3000}
+        visibleSlides={3}
+      >
+        <Slider>
+          <Slide index={0}>
+            <Image src="img/html-5.svg" alt="" />
+          </Slide>
+          <Slide index={1}>
+            <Image src="img/css3.svg" alt="" />
+          </Slide>
+          <Slide index={2}>
+            <Image src="img/javascript.svg" alt="" />
+          </Slide>
+          <Slide index={3}>
+            <Image src="img/react.svg" alt="" />
+          </Slide>
+          <Slide index={4}>
+            <Image src="img/nodejs.svg" alt="" />
+          </Slide>
+          <Slide index={5}>
+            <Image src="img/angularjs.svg" alt="" />
+          </Slide>
+          <Slide index={6}>
+            <Image src="img/mongodb.svg" alt="" />
+          </Slide>
+          <Slide index={7}>
+            <Image src="img/mysql.svg" alt="" />
+          </Slide>
+          <Slide index={8}>
+            <Image src="img/bootstrap.svg" alt="" />
+          </Slide>
+          <Slide index={9}>
+            <Image src="img/wordpress.svg" alt="" />
+          </Slide>
+        </Slider>
+      </CarouselProvider>
     </Wrapper>
   );
 }
